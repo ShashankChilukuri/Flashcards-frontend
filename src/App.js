1f2bc1dc1ef61d5ebcf63a074300f8828d7875bf
ChilukuriShashank <155829699+ChilukuriShashank@users.noreply.github.com>
2024-08-12 22:23:53 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Flashcards from './Home';
+import Dashboard from './Dashboard';
+import AdminLogin from './AdminLogin';
 
-function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true); // Set authentication status to true
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false); // Set authentication status to false
+  };
+
+  const ProtectedRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Flashcards />} />
+        <Route path="/login" element={<AdminLogin onLogin={handleLogin} />} />
+        <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard onLogout={handleLogout} />} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
